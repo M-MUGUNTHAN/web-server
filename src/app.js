ce80f2aba14b28fa16047f62a1603cf84bfd42fa
M-MUGUNTHAN <52280716+M-MUGUNTHAN@users.noreply.github.com>
2020-12-24 17:18:40 +0530

    second commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 const express=require("express")
 const app=express();
+const port=process.env.PORT||3000;
 app.get("/",(req,res)=>{
     res.send("<h1>hello from express</h1>")
 })
 app.get("/help",(req,res)=>{
     res.send("help page")
 })
-app.listen(3000,()=>{
-    console.log("running in port 3000")
+app.listen(port,()=>{
+    console.log("running in port "+port)
 })
